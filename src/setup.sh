e8d995d6f883fd1b20a73d79fe6bceeb02a7cd9d
Gaurav Tyagi <gtyagi@stanford.edu>
2025-11-20 09:14:53 -0800

    setup attempt

--- src/setup.sh
+++ src/setup.sh
@@ -8,20 +8,21 @@ set -e
 # conda activate mindeye
 # conda install numpy matplotlib tqdm scikit-image jupyterlab -y
 # conda install -c conda-forge accelerate -y
-
+module load python/3.12.1
 python3 -m venv mindeye
 source mindeye/bin/activate
-
 pip install --upgrade pip setuptools wheel
 
-pip install numpy matplotlib tqdm scikit-image jupyterlab
-pip install accelerate
+#pip install opencv-python-headless==4.6.0.66 --only-binary :all:
+pip install -c constraints.txt numpy matplotlib tqdm scikit-image jupyterlab
+
+pip install -c constraints.txt accelerate
 
 pip install clip-retrieval webdataset clip pandas matplotlib ftfy regex kornia umap-learn
 pip install dalle2-pytorch
 
-pip install torchvision==0.15.2 torch==2.0.1
-pip install diffusers==0.13.0
+pip install -c constraints.txt torchvision==0.15.2 torch==2.0.1
+pip install -c constraints.txt diffusers==0.13.0
 
-pip install info-nce-pytorch==0.1.0
-pip install pytorch-msssim
+pip install -c constraints.txt info-nce-pytorch==0.1.0
+pip install -c constraints.txt pytorch-msssim
